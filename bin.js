8036a011b3b4cc445a45475d9fac18175b22f971
Scott Green <sagreenxyz@gmail.com>
2022-03-20 19:41:23 -0500

    3. fix to spawn in windows error

--- bin.js
+++ bin.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const open = require('open')
-const { exec, spawn } = require('child_process')
-exec('npx webpack --mode="development"', () => {
-    spawn('npx', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
+const spawn = require('cross-spawn');
+const { exec } = require('child_process')
+exec('npx webpack --mode=development', () => {
+    spawn('npx', [ 'webpack', '--watch', '--mode=development' ], { stdio: 'inherit' });
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
